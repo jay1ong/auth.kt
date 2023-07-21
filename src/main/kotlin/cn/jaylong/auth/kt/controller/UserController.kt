4b54755b0bc80f4ea8e579f3b0b1cc2120ca6676
I'm <13693004+jay1ong@users.noreply.github.com>
2023-07-21 16:59:53 +0800

    feat: user list

--- src/main/kotlin/cn/jaylong/auth/kt/controller/UserController.kt
+++ src/main/kotlin/cn/jaylong/auth/kt/controller/UserController.kt
@@ -3,6 +3,7 @@ package cn.jaylong.auth.kt.controller
 import cn.jaylong.auth.kt.po.User
 import cn.jaylong.auth.kt.repository.UserJpaRepository
 import io.swagger.annotations.ApiOperation
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -19,11 +20,15 @@ class UserController(private var repository: UserJpaRepository) {
 
 
     @ApiOperation("根据用户名获取用户信息")
-    @GetMapping(
-        "/username",
-        produces = ["application/json"]
-    ) //    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') ")
+    @GetMapping("/username") //    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') ")
     fun getUserByUsername(@RequestParam username: String): User {
         return repository.loadUserByUsername(username)
+    }
+
+    @ApiOperation("获取用户列表")
+    @GetMapping("/users")
+    @PreAuthorize("hasRole('ADMIN')")
+    fun listUser(): List<User> {
+        return repository.findAll()
     }
 }
